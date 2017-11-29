3557c43d51217158aa5ae11c032d17adfc867b8d
marskr <marskr@opoczta.pl>
2017-11-30 00:49:19 +0100

    singleton containing settings added

--- iRecon/iRecon/DataManager/DataManager.cs
+++ iRecon/iRecon/DataManager/DataManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Data;
 
 namespace iRecon.DataManager
@@ -20,6 +21,30 @@ namespace iRecon.DataManager
             }
 
             throw new Exception("The text is not a number");
+        }
+    }
+    public sealed class SettingsContainer
+    {
+        private static SettingsContainer SingletonInstance = null;
+        private static readonly object Lock = new object();
+
+        public const int i_testImageOffset = 95, i_basicImageOffset = 77;
+        public string s_Path { get { return GetPath(); } }
+        public string GetPath() { return Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location); }
+
+        public static SettingsContainer Instance
+        {
+            get
+            {
+                lock (Lock)
+                {
+                    if (SingletonInstance == null)
+                    {
+                        SingletonInstance = new SettingsContainer();
+                    }
+                    return SingletonInstance;
+                }
+            }
         }
     }
 }
--- iRecon/iRecon/MainWindow.xaml.cs
+++ iRecon/iRecon/MainWindow.xaml.cs
@@ -8,54 +8,50 @@ using System.Windows;
 using System.IO;
 using System;
 using iRecon.LogManager;
-using System.Threading;
-using System.Threading.Tasks;
+using iRecon.DataManager;
 
 namespace iRecon
 {
     public partial class MainWindow : Window
     {
-        private static string s_Path { get { return GetPath(); } }
-        private static string GetPath() { return Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location); }
-
         // Custom class to store image scores
         class WeightedImages
         {
-            public string ImagePath { get; set; } = "";
-            public string ImageBasicPath { get; set; } = "";
-            public long Score { get; set; } = 0;
+            public string s_ImagePath { get; set; } = string.Empty;
+            public string s_ImageBasicPath { get; set; } = string.Empty;
+            public long l_Score { get; set; } = 0;
         }
 
         // A List<> which contains processed images scores
-        List<WeightedImages> imgList = new List<WeightedImages>();
-        List<string> l_basicImgPath;
+        List<WeightedImages> l_imgList = new List<WeightedImages>();
+        List<string> l_basicImgPath = new List<string>();
 
-        private void ProcessFolder(string mainFolder, string basicImage)
+        private void ProcessFolder(string s_mainFolder, string s_basicImage)
         {
-            foreach (var file in System.IO.Directory.GetFiles(mainFolder))
-                ProcessImage(file, basicImage);
+            foreach (string file in System.IO.Directory.GetFiles(s_mainFolder))
+                ProcessImage(file, s_basicImage);
 
-            foreach (var dir in System.IO.Directory.GetDirectories(mainFolder))
-                ProcessFolder(dir, basicImage);
+            foreach (string dir in System.IO.Directory.GetDirectories(s_mainFolder))
+                ProcessFolder(dir, s_basicImage);
         }
 
-        private void ProcessImage(string testImage, string basicImage)
+        private void ProcessImage(string s_testImage, string s_basicImage)
         {
             ErrInfLogger.LockInstance.InfoLog("Start of the ProcessImage");
-            if (testImage == basicImage)
+            if (s_testImage == s_basicImage)
             {
-                ErrInfLogger.LockInstance.InfoLog("End of the ComputeImages");
+                ErrInfLogger.LockInstance.InfoLog("End of the ProcessImage");
                 return;
             }
 
             try
             {
-                long score;
-                long matchTime;
+                long l_score;
+                long l_matchTime;
 
-                var pair = ComputeImages(testImage, basicImage);
-                score = pair.Key;
-                matchTime = pair.Value;
+                KeyValuePair <long, long> pair = ComputeImages(s_testImage, s_basicImage);
+                l_score = pair.Key;
+                l_matchTime = pair.Value;
             }
             catch (Exception ex)
             {
@@ -66,31 +62,36 @@ namespace iRecon
             ErrInfLogger.LockInstance.InfoLog("End of the ProcessImage");
         }
 
-        KeyValuePair<long, long> ComputeImages(string testImage, string basicImage)
+        KeyValuePair<long, long> ComputeImages(string s_testImage, string s_basicImage)
         {
             ErrInfLogger.LockInstance.InfoLog("Start of the ComputeImages");
-            long score;
-            long matchTime;
-            using (Mat modelImage = CvInvoke.Imread(basicImage, ImreadModes.Color))
+            long l_score;
+            long l_matchTime;
+            using (Mat modelImage = CvInvoke.Imread(s_basicImage, ImreadModes.Color))
             {
-                using (Mat observedImage = CvInvoke.Imread(testImage, ImreadModes.Color))
+                using (Mat observedImage = CvInvoke.Imread(s_testImage, ImreadModes.Color))
                 {
                     Mat homography;
                     VectorOfKeyPoint modelKeyPoints;
                     VectorOfKeyPoint observedKeyPoints;
 
-                    using (var matches = new VectorOfVectorOfDMatch())
+                    using (VectorOfVectorOfDMatch matches = new VectorOfVectorOfDMatch())
                     {
                         Mat mask;
-                        DrawMatches.FindMatch(modelImage, observedImage, out matchTime, out modelKeyPoints, out observedKeyPoints, matches,
-                           out mask, out homography, out score);
+                        DrawMatches.FindMatch(modelImage, observedImage, out l_matchTime, out modelKeyPoints,
+                                              out observedKeyPoints, matches, out mask, out homography, out l_score);
                     }
 
-                    imgList.Add(new WeightedImages() { ImagePath = testImage, ImageBasicPath = basicImage, Score = score });
+                    l_imgList.Add(new WeightedImages()
+                    {
+                        s_ImagePath = s_testImage.Remove(0, SettingsContainer.i_testImageOffset),
+                        s_ImageBasicPath = s_basicImage.Remove(0, SettingsContainer.i_basicImageOffset),
+                        l_Score = l_score
+                    });
                 }
             }
             ErrInfLogger.LockInstance.InfoLog("End of the ComputeImages");
-            return new KeyValuePair<long, long>(score, matchTime);
+            return new KeyValuePair<long, long>(l_score, l_matchTime);
         }
 
         public MainWindow()
@@ -99,36 +100,27 @@ namespace iRecon
 
             InitializeComponent();
 
-            // Source image, small portion of image to be found in bigger pictures
-            l_basicImgPath = new List<string>();
-            l_basicImgPath.Add(s_Path + @"\reco_settings\img\dog\basic.jpg");
-            l_basicImgPath.Add(s_Path + @"\reco_settings\img\cat\basic.jpg");
-            l_basicImgPath.Add(s_Path + @"\reco_settings\img\horse\basic.jpg");
-            l_basicImgPath.Add(s_Path + @"\reco_settings\img\rat\basic.jpg");
-
             ErrInfLogger.LockInstance.InfoLog("Aplication finished without major errors");
         }
+
         void btn_DataProcess(object sender, RoutedEventArgs e)
         {
             ErrInfLogger.LockInstance.InfoLog("Start of the DataProcess");
             try
             {
-                foreach (var element in l_basicImgPath)
-                {
-                    ProcessImage(s_Path + @"\test\test.jpg", element);
-                }
-
-                ResultsTable.ItemsSource = imgList.OrderByDescending(x => x.Score).ToList();
+                ProcessFolder(SettingsContainer.Instance.s_Path + @"\reco_settings\img",
+                              SettingsContainer.Instance.s_Path + @"\test\test.jpg");
+                l_imgList = l_imgList.OrderByDescending(x => x.l_Score).ToList();
+                ResultsTable.ItemsSource = l_imgList;
             }
             catch (Exception ex)
             {
                 MessageBox.Show("THE PROBLEM OCCURED WITH DATA PROCESSING!");
                 ErrInfLogger.LockInstance.ErrorLog(ex.ToString());
-                //MessageBox.Show(ex.ToString());
             }
-            ResultsTable.ItemsSource = imgList.OrderByDescending(x => x.Score).ToList();
             ErrInfLogger.LockInstance.InfoLog("End of the DataProcess");
         }
+
         void btn_DataShow(object sender, RoutedEventArgs e)
         {
             ErrInfLogger.LockInstance.InfoLog("Start of the DataShow");
@@ -136,16 +128,17 @@ namespace iRecon
             {
                 if ((ResultsTable.Items.Count > 0) && (ResultsTable.Columns.Count > 0))
                 {
-                    string imgPath = s_Path + @"\test\" + txtPhotoPath.Text + @".jpg";
+                    string s_imgPath = SettingsContainer.Instance.s_Path + @"\test\" + txtPhotoPath.Text + @".jpg";
 
-                    long score;
-                    long matchTime;
+                    long l_score;
+                    long l_matchTime;
 
-                    using (Mat modelImage = CvInvoke.Imread(l_basicImgPath[0], ImreadModes.Color))
-                    using (Mat observedImage = CvInvoke.Imread(imgPath, ImreadModes.Color))
+                    using (Mat modelImage = CvInvoke.Imread(SettingsContainer.Instance.s_Path + @"\reco_settings\img" + 
+                                                            l_imgList[0].s_ImagePath, ImreadModes.Color))
+                    using (Mat observedImage = CvInvoke.Imread(s_imgPath, ImreadModes.Color))
                     {
-                        var result = DrawMatches.Draw(modelImage, observedImage, out matchTime, out score);
-                        var iv = new emImageViewer(result, score);
+                        Mat result = DrawMatches.Draw(modelImage, observedImage, out l_matchTime, out l_score);
+                        emImageViewer iv = new emImageViewer(result, l_score);
                         iv.Show();
                     }
                 }
@@ -153,10 +146,33 @@ namespace iRecon
             catch (Exception ex)
             {
                 MessageBox.Show("DID YOU PROVIDE PROPER PATH TO TEXT BOX?\n" +
-                                "IN EXAMPLE FOR 'test.jpg' FILE YOU TYPE IN TEXT BOX JUST 'test'.");
+                                "IN EXAMPLE FOR 'test.jpg' FILE YOU TYPE IN TEXT BOX JUST 'test'!");
                 ErrInfLogger.LockInstance.ErrorLog(ex.ToString());
             }
             ErrInfLogger.LockInstance.InfoLog("End of the DataShow");
+        }
+
+        void btn_Predict(object sender, RoutedEventArgs e)
+        {
+            string s_choice = l_imgList[0].s_ImagePath.Remove(0, 1);
+            switch (new string(s_choice.Take(3).ToArray()))
+            {
+                case "dog":
+                    MessageBox.Show("My predict is: DOG!!!");
+                    break;
+                case "cat":
+                    MessageBox.Show("My predict is: CAT!!!");
+                    break;
+                case "rat":
+                    MessageBox.Show("My predict is: RAT!!!");
+                    break;
+                case "hor":
+                    MessageBox.Show("My predict is: HORSE!!!");
+                    break;
+                default:
+                    MessageBox.Show("Some kind of an error...");
+                    break;
+            }
         }
     }
 }
